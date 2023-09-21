9f99348218be9631307f042fcc396146b8d81886
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2023-09-21 20:45:59 +0300

    cw2109

--- src/com/samsung/cw/Main.java
+++ src/com/samsung/cw/Main.java
@@ -1,7 +1,16 @@
 package com.samsung.cw;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int intValue1 = scanner.nextInt();
+
+
+        System.out.print(intValue1 + ", ");
+        System.out.print(intValue1);
     }
 }
